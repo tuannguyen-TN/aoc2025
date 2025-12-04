e5a4963e4e6da63d1fd37cfa701ff1cd9639ba04
Tuan Nguyen <tuannguyen221101@gmail.com>
2025-12-03 22:45:03 -0500

    finished day 1

--- day-1/main.py
+++ day-1/main.py
@@ -1,22 +1,21 @@
-def rotate(currNum, direction, steps):
-    if direction == "R":
-        currNum += steps
-    else:
-        currNum -= steps
-
-    return currNum % 100
-
-
 num = 50
 total = 0
 
 with open("input.txt", "r") as f:
     content = f.readlines()
     for instruction in content:
-        dir, steps = instruction[0], instruction[1:]
-        num = rotate(num, dir, int(steps))
-        if num == 0:
-            total += 1
-        total += steps // 100
+        dir, steps = instruction[0], int(instruction[1:])
+
+        while steps > 0:
+            if dir == "R":
+                num += 1
+            else:
+                num -= 1
+
+            num %= 100
+            if num == 0:
+                total += 1
+
+            steps -= 1
 
 print(total)
