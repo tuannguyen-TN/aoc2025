e6e7d0d3ebfd79247837c6c26fa39f6023b0c66f
Tuan Nguyen <tuannguyen221101@gmail.com>
2025-12-05 08:39:20 -0500

    initial code for day 3

---
+++ day-3/main.py
@@ -0,0 +1,33 @@
+import math
+
+def getJoltage(bank):
+    lastDigit = bank % 10
+    bank //= 10
+    
+    prevLargest = lastDigit
+
+    lastDigit = bank % 10
+    bank //= 10
+
+    currLargest = lastDigit
+
+    while bank > 0:
+       lastDigit = bank % 10
+       bank //= 10
+
+       if lastDigit >= currLargest:
+           currLargest, prevLargest = lastDigit, currLargest
+
+    return currLargest * 10 + prevLargest
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        content = list(map(int, f.readlines()))
+        total = 0
+
+        for num in content:
+            print(num, getJoltage(num))
+            total += getJoltage(num)
+
+        print(total)
