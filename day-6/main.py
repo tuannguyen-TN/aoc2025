f9c91dff5b272c4df5f0ed0ca681a145d6bad1c0
Tuan Nguyen <tuannguyen221101@gmail.com>
2025-12-14 10:36:07 -0500

    wip part 2

--- day-6/main.py
+++ day-6/main.py
@@ -5,17 +5,19 @@ with open("input.txt", "r") as f:
         tempArr = []
 
         if i != len(content) - 1:
-            left, right = 0, 0
+            # left = 0
+            #
+            # while left < len(content[i]):
+            #     while left < len(content[i]) and not content[i][left].isnumeric():
+            #         left += 1
+            #
+            #     for right in range(left, len(content[i])):
+            #         if not content[i][right].isnumeric():
+            #             tempArr.append(content[i][left:right])
+            #             left = right
+            #             break
 
-            while left < len(content[i]):
-                while left < len(content[i]) and not content[i][left].isnumeric():
-                    left += 1
-
-                for right in range(left, len(content[i])):
-                    if not content[i][right].isnumeric():
-                        tempArr.append(int(content[i][left:right]))
-                        left = right
-                        break
+            tempArr = content[i].strip("\n")
 
         else:
             for j in range(len(content[i])):
@@ -24,20 +26,48 @@ with open("input.txt", "r") as f:
 
         matrix.append(tempArr)
 
-    ROWS, COLS = len(matrix), len(matrix[0])
+    print(matrix)
+
     total = 0
 
-    for col in range(COLS):
-        operation = lambda x, y: x + y
-        startingValue = 0
+    
 
-        if matrix[ROWS - 1][col] == "*":
-            operation = lambda x,y: x * y
-            startingValue = 1
-
-        for row in range(ROWS - 1):
-            startingValue = operation(startingValue, matrix[row][col])
-
-        total += startingValue
-
+    # ROWS, COLS = len(matrix), len(matrix[0])
+    # total = 0
+    #
+    # for col in range(COLS):
+    #     operation = lambda x, y: x + y
+    #     startingValue = 0
+    #
+    #     if matrix[ROWS - 1][col] == "*":
+    #         operation = lambda x,y: x * y
+    #         startingValue = 1
+    #
+    #     pos = 0
+    #
+    #     while True:
+    #         digitFound = False
+    #         localColValue = 0
+    #
+    #         for row in range(ROWS - 1):
+    #             if pos < len(matrix[row][col]):
+    #                 localColValue = localColValue * 10 + int(matrix[row][col][::-1][pos])
+    #                 digitFound = True
+    #
+    #         print(localColValue)
+    #
+    #         if digitFound:
+    #             startingValue = operation(startingValue, localColValue)
+    #         else:
+    #             break
+    #
+    #         pos += 1
+    #
+    #     total += startingValue
+    #
+    #     for row in range(ROWS - 1):
+    #         startingValue = operation(startingValue, matrix[row][col])
+    #
+    #     total += startingValue
+    #
     print(total)
